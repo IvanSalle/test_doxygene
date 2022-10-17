c3efe047574489076d6787d36d7d4002002fe6a7
Ivan Salle <isalle@iutbayonne.univ-pau.fr>
2022-10-17 15:19:32 +0200

    ajout du code

---
+++ docs/search/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "imnprstv~",
+  1: "iv",
+  2: "imrv",
+  3: "imstv~",
+  4: "imnp",
+  5: "t"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Pages"
+};
+
